0371b59170eba0cce6133385ee10c3397737a7e1
breaddudeonsite <breaddudeonsite@gmail.com>
2026-09-18 22:06:29 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Inkling service worker: app shell works offline. Bump VERSION when you change any cached file. */
-const VERSION = 'inkling-v2';
+const VERSION = 'inkling-v3';
 const SHELL = [
   './',
   './index.html',
